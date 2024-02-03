2ded21eb540b023d51100315d158ce6f939425f1
MaksymUK <clear>
2024-02-03 21:57:57 +0000

    add CourseListView & CourseDetailView

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views import generic
 
 from .models import Course
 
@@ -7,10 +9,21 @@ from .models import Course
 def index(request: HttpRequest) -> HttpResponse:
     courses = Course.objects.all()[0:3]
     context = {
-        "courses": courses
+        "courses": courses,
+        'transparent_header': True,
     }
     return render(request, "website/index.html", context=context)
 
 
 def contact_us(request: HttpRequest) -> HttpResponse:
     return render(request, "website/contact_us.html")
+
+
+class CourseListView(generic.ListView):
+    model = Course
+    queryset = Course.objects.all()
+    paginate_by = 5
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course

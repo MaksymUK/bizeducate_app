4cee7e0ae7e56cca80b03c8acc9a58faf6e0115c
MaksymUK <clear>
2024-02-03 21:53:31 +0000

    new path for course-list & course-detail view

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index, contact_us
+from .views import index, contact_us, CourseListView, CourseDetailView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("contact_us/", contact_us, name="contact-us")
+    path("contact_us/", contact_us, name="contact-us"),
+    path("courses/", CourseListView.as_view(), name="course-list"),
+    path("courses/<int:pk>/", CourseDetailView.as_view(), name="course-detail")
 ]
 
 app_name = "website"

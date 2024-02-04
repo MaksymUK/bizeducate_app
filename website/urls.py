2a8a29d17eaf25bcb2d25779d618dc9c100583a8
MaksymUK <clear>
2024-02-04 08:14:26 +0000

    add FinanceListView, url path & html to filter one category courses

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, contact_us, CourseListView, CourseDetailView
+from .views import index, contact_us, CourseListView, CourseDetailView, FinanceListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact_us/", contact_us, name="contact-us"),
     path("courses/", CourseListView.as_view(), name="course-list"),
-    path("courses/<int:pk>/", CourseDetailView.as_view(), name="course-detail")
+    path("courses/<int:pk>/", CourseDetailView.as_view(), name="course-detail"),
+    path("finance/", FinanceListView.as_view(), name="finance-list"),
 ]
 
 app_name = "website"
--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest, HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .models import Course
+from .models import Course, Trainer
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -24,6 +24,20 @@ class CourseListView(generic.ListView):
     queryset = Course.objects.all()
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = set(course.category for course in context['course_list'])
+        context['trainers'] = Trainer.objects.filter(category__in=categories)
+        return context
+
 
 class CourseDetailView(generic.DetailView):
     model = Course
+
+
+class FinanceListView(CourseListView):
+    template_name = "website/finance_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(category__name__iexact="finance")
+

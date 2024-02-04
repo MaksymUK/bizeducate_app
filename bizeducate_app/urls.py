6a940028f615cb6ac2f746191266367b39fd5681
MaksymUK <clear>
2024-02-04 10:12:26 +0000

    add MEDIA_ROOT in project urls.py

--- bizeducate_app/urls.py
+++ bizeducate_app/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("website.urls", namespace="website"))
+    path("", include("website.urls", namespace="website")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
